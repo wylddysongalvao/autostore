670311d7f8c2fa55b32e6082bf51c8285a4dbec4
HELP-DESK-12 <wylddyson@NETCOM.PSI.BR>
2022-01-06 10:52:35 -0300

    Atualizando

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://10.10.13.22:8080/",
+    baseURL: "",
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json"
